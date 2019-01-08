d5e4c0c4022f15f9d292b156613c2f8f2b461baa
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2019-01-07 23:54:47 -0500

    Added class syntax

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 var plugins = [
   "@babel/plugin-transform-arrow-functions",
-  "@babel/plugin-transform-template-literals"
+  "@babel/plugin-transform-template-literals",
+  "@babel/plugin-transform-classes"
 ];
 
 var presets = [["@babel/preset-env"]];
--- src/classes/Test.js
+++ src/classes/Test.js
@@ -5,40 +5,42 @@
 /**
  *
  */
-function Test() {
-  this.result = 0;
-  this.errors = [];
+class Test {
+  constructor() {
+    this.result = 0;
+    this.errors = [];
+  }
+
+  /**
+   *
+   * @param {boolean} isValid If the test passed, 'isValid' equals 'true'.
+   * @param {number} value The points that the student is awarded, if 'isValid' equals 'true'.
+   * @param {string} errorMsg The error message.
+   */
+  assert(isValid, value, errorMsg) {
+    if (isValid) this.result += value;
+    else
+      this.errors.push({
+        msg: errorMsg,
+        value: value
+      });
+    return this;
+  }
+
+  /**
+   *
+   */
+  hasErrors() {
+    return this.errors.length > 0;
+  }
+
+  /**
+   *
+   */
+  displayErrors() {
+    return this.errors.reduce(
+      (str, err) => formatString("$1 (-$2%)\n", str + err.msg, err.value),
+      ""
+    );
+  }
 }
-
-/**
- *
- * @param {boolean} isValid If the test passed, 'isValid' equals 'true'.
- * @param {number} value The points that the student is awarded, if 'isValid' equals 'true'.
- * @param {string} errorMsg The error message.
- */
-Test.prototype.assert = function(isValid, value, errorMsg) {
-  if (isValid) this.result += value;
-  else
-    this.errors.push({
-      msg: errorMsg,
-      value: value
-    });
-  return this;
-};
-
-/**
- *
- */
-Test.prototype.hasErrors = function() {
-  return this.errors.length > 0;
-};
-
-/**
- *
- */
-Test.prototype.displayErrors = function() {
-  return this.errors.reduce(
-    (str, err) => formatString("$1 (-$2%)\n", str + err.msg, err.value),
-    ""
-  );
-};
